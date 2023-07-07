23ec2ca9ff99ff6111045729100fca8c97c82454
Shwetank Singh <ssingh.21.2019@gmail.com>
2023-07-07 14:50:47 +0530

    uploads

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
- let menuBtn = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.header .flex .navbar');
-
- menuBtn.onclick = () => {
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-  }
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
 
 
     var swiper = new Swiper(".mySwiper", {
@@ -72,3 +78,5 @@ var swiper = new Swiper(".course-slider", {
       },
   });
   
+
+ 
